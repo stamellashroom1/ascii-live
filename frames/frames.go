4cd2908831b9b9d0af51f97bbf669b76bd079405
stamellashroom1 <alex@crpl.com.au>
2025-10-07 15:58:23 +1100

    create apple.go

--- frames/frames.go
+++ frames/frames.go
@@ -39,6 +39,7 @@ func DefaultFrameType(frames []string) FrameType {
 }
 
 var FrameMap = map[string]FrameType{
+	"apple":           Apple,
 	"batman":          Batman,
 	"batman-running":  BNR,
 	"bnr":             BNR,
